86b1bdbf571885ed6b4d0541f2db7c15e3dc0460
Luis Angel <angelfgdeveloper@gmail.com>
2024-09-01 14:39:23 -0600

    Creacion de servidor express

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,14 @@
+import { Server } from './presentation/server';
 
-console.log('Hola Mundo!!');
+(async () => {
+  await main();
+})();
+
+async function main() {
+  // await base de datos
+
+  // inicio del server
+  new Server({
+    port: 3000
+  }).start();
+}
