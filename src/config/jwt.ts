d96dba3597c3a2e9c63e638b9d657dc7d6e2e941
Luis Angel <angelfgdeveloper@gmail.com>
2024-09-07 14:25:44 -0600

    Generando jwt

---
+++ src/config/jwt.ts
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+
+export class JwtAdapter {
+
+  static async generateToken(
+    payload: Object, 
+    duration: string = '2h'
+  ): Promise<string | null> {
+
+    return new Promise((resolve, reject) => {
+
+      // generacion del seed
+      jwt.sign(payload, 'SEED', { expiresIn: duration }, (err, token) => {
+
+        if ( err ) return resolve(null);
+
+        resolve(token!);
+        
+      });
+
+    });
+    
+  }
+
+}
