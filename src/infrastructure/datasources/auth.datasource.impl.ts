0aa3485148bd140cfa182c066ab0832823254b84
Luis Angel <angelfgdeveloper@gmail.com>
2024-09-07 14:06:47 -0600

    Generando mapper de user

--- src/infrastructure/datasources/auth.datasource.impl.ts
+++ src/infrastructure/datasources/auth.datasource.impl.ts
@@ -1,6 +1,7 @@
 import { BcryptAdapter } from '../../config';
 import { UserModel } from '../../data/mongodb';
 import { AuthDatasource, CustomError, RegisterUserDto, UserEntity } from '../../domain';
+import { UserMapper } from '../mappers/user.mapper';
 
 type HashFunction = (password: string) => string;
 type CompareFunction = (password: string, hashed: string) => boolean;
@@ -32,16 +33,7 @@ export class AuthDatasourceImpl implements AuthDatasource {
       await user.save();
 
       // 3. Mapear la respuesta a nuestra entidad
-      
-      // falta un mapper
-      return new UserEntity(
-        user.id,
-        name,
-        email,
-        user.password,
-        user.roles,
-      );
-
+      return UserMapper.userEntityFromObject(user);
     } catch (error) {
 
       if (error instanceof CustomError) {
