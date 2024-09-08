791dd68367f3fa177c609b7ce0cc62ae9c2068ab
Luis Angel <angelfgdeveloper@gmail.com>
2024-09-08 12:27:15 -0600

    Use case de registro de usuario

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -6,3 +6,6 @@ export * from './errors/custom.error';
 
 export * from './repositories/auth.repository';
 export * from './datasources/auth.datasource';
+
+export * from './use-cases/auth/register-user.use-case';
+// export * from './use-cases/auth/login-user.use-case';
